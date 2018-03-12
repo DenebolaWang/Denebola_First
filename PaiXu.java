14e7f4307c8b30c201bc813e1e848d5ab3b1c24f
DenebolaWang <37263056+DenebolaWang@users.noreply.github.com>
2018-03-12 06:23:12 +0000

    bubbleSort

--- PaiXu.java
+++ PaiXu.java
@@ -1,3 +1,4 @@
+//对最基本冒泡排序的简单优化
 public class PaiXu {
 public static void main(String[] args) {
 	int paixu[]= {2,333,44,555,6,777,88,9,10};
@@ -7,21 +8,21 @@ public static void main(String[] args) {
 public static void bubbleSort(int[] array, int left, int right) {
 	while(right>left) 
     {
-		//初始化c(c用来定位最后一次发生交换的位置，之后的数已被排序，不需重复判断)
-		int c=left;
+		//初始化Denebola(Denebola用来定位最后一次发生交换的位置，之后的数已被排序，不需重复判断)
+		int Denebola=left;
         for(int i=left+1;i<=right;i++)
         {
         	if(array[i-1]>array[i])
         	{
-			//交换位置并定位c
+			//交换位置并定位Denebola
         		int a=array[i-1];
         		array[i-1]=array[i];
         		array[i]=a;
-        		c=i-1;
+        		Denebola=i-1;
         	}
         }
-        //根据c确定下一次排序的范围
-    	right=c;
+        //根据Denebola确定下一次排序的范围
+    	right=Denebola;
     }
 }
 }
